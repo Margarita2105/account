de1f6f1294fb39f182e667d3cff1711af8db27a1
margarita2105 <khokhlova2105@yandex.ru>
2021-01-13 22:06:58 +0300

    c

---
+++ account/api/views.py
@@ -0,0 +1,70 @@
+from django.core.mail import send_mail
+
+from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+
+from .serializers import UserSerializer
+
+from users.models import User
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    filterset_fields = ['username']
+    ordering = ['id']
+    lookup_field = 'username'
+
+
+class MyProfile(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
+
+
+class GetToken(APIView):
+
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        try:
+            user = User.objects.get(email=email, password=password)
+            user.is_active = True
+            user.save()
+            return Response({'token': user.get_token()})
+
+        except User.DoesNotExist:
+            return Response(
+                data={'detail': 'Пользователя с такими данными не существует'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+
+class RegistrationView(APIView):
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+
+        if serializer.is_valid():
+            email = serializer.data.get('email')
+            username = serializer.data.get('username')
+            password = serializer.data.get('password')
+            User.objects.update_or_create(
+                defaults={'password': password},
+                email=email, username=username
+            )
+
+            send_mail(
+                'Registration',
+                'Your password is ' + str(password),
+                'from@gmail.com',
+                [str(email)],
+                fail_silently=False,
+            )
+            return Response({'details': 'Ваши данные высланы вам на почту'},
+                status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
---
+++ account/users/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.core import validators
+
+
+class User(AbstractUser):
+    class UserStatus:
+        EXECUTOR = 'executor'
+        CUSTOMER = 'customer'
+
+        choices = [
+            (EXECUTOR, EXECUTOR),
+            (CUSTOMER, CUSTOMER),
+        ]
+
+    role = models.CharField(max_length=9, choices=UserStatus.choices, default=UserStatus.EXECUTOR)
+    bio = models.CharField(max_length=255, blank=True, null=True)
+    email = models.EmailField(
+        validators=[validators.validate_email],
+        unique=True,
+        blank=False
+        )
+    balance = models.IntegerField(blank=True, null=True)
+    freeze_balance = models.IntegerField(blank=True, null=True)
+    password = models.CharField(max_length=9)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def get_token(self):
+        refresh = RefreshToken.for_user(self)
+        token = str(refresh.access_token)
+        return token
